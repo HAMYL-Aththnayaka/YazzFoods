13c6428c6eb81eca7f1cd13bf3e226956c3ec7bd
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-03-17 12:00:22 +0530

    Learned : font styles and text styles

--- YazzFoods/tailwind.config.js
+++ YazzFoods/tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      "./src/**/*.{js,jsx,ts,tsx}"
+    "./public/**/*.html",        // Add this to watch your HTML
+    "./public/**/*.{js,ts}",    // Add this if you have JS in public
+    "./src/**/*.{js,jsx,ts,tsx}" 
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
